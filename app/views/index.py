6ea91453319365d047d96763d0e311c7bef42245
Vaayne <lyishaou@gmail.com>
2016-08-03 00:18:58 +0800

    改进数据源更新方式，加快首页加载

--- app/views/index.py
+++ app/views/index.py
@@ -12,7 +12,6 @@ from ..func import update_articles
 @view.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    # update_articles.update()
     items = db.posts.find().sort('post_time', DESCENDING).limit(10)
     # for item in items:
     #     print item.get('post_time'), type(item['post_time'])
@@ -20,3 +19,7 @@ def index():
     return render_template('index.html', items=items)
 
 
+@view.route('/update')
+def update():
+    update_articles.update()
+    return '<h1>更新数据成功</h1>'
