b743c70b8ff0d3d67c9068bd9538c196b27160d5
Vaayne <lyishaou@gmail.com>
2016-08-05 01:13:21 +0800

    增加分类显示

--- app/views/index.py
+++ app/views/index.py
@@ -20,3 +20,17 @@ def index():
 def update():
     update_articles.update()
     return '<h1>更新数据成功</h1>'
+
+
+@view.route('/wx/<aid>')
+def category_wx(aid):
+    page = request.args.get('page', 1, type=int)
+    items = db.posts.find({'spider_name': 'wx', 'aid': aid}).skip(10 * (page - 1)).sort('post_time', DESCENDING).limit(10)
+    return render_template('index.html', items=items, page=page)
+
+
+@view.route('/category/<kind>')
+def category(kind):
+    page = request.args.get('page', 1, type=int)
+    items = db.posts.find({'spider_name': kind}).skip(10 * (page - 1)).sort('post_time', DESCENDING).limit(10)
+    return render_template('index.html', items=items, page=page)
